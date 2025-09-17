1d36b8f703b489bd01d72cc593c33a33351fc91e
Kaique Souza <kaique.souza3101@gmail.com>
2025-09-17 02:25:14 -0300

    feat: add efeitos e cores

--- js/about.js
+++ js/about.js
@@ -7,7 +7,7 @@ let slideAtual = 0;
 const cards = containerCards.querySelectorAll('.card-membro');
 const totalCards = cards.length;
 
-const totalIndicadores = 2;
+const totalIndicadores = 3;
 
 for (let i = 0; i < totalIndicadores; i++) {
     const indicador = document.createElement('div');
